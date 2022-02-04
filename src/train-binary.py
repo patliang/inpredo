42138a4a2e4ed6fa75e0f36a783abfeeaf61d871
JMLJEL <pat.liang@gmail.com>
2022-02-03 20:04:03 -0500

    Update train-binary.py

--- src/train-binary.py
+++ src/train-binary.py
@@ -6,6 +6,7 @@ import sys
 
 # os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
+from tensorflow import keras
 from keras import optimizers
 from keras.layers import Dropout, Flatten, Dense, Activation, BatchNormalization
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, Conv2D
@@ -28,14 +29,14 @@ else:
 # BEFORE STARTING TRAINING YOU NEED TO MANUALLY TAKE 20 PERCENENT OF THE TRAINING DATA AND PUT IT INTO VALIDATION FOLDER
 # I was too lazy to do it in the code.
 
-train_data_dir = './data/train/'
-validation_data_dir = './data/validation/'
+train_data_dir = '../data/train/'
+validation_data_dir = '../data/validation/'
 
 # Input the size of your sample images
 img_width, img_height = 150, 150
 # Enter the number of samples, training + validation
-nb_train_samples = 13204
-nb_validation_samples = 1412
+nb_train_samples = 6856
+nb_validation_samples = 1666
 nb_filters1 = 32
 nb_filters2 = 32
 nb_filters3 = 64
@@ -49,18 +50,19 @@ batch_size = 128
 lr = 0.001
 chanDim =3
 
+
 model = Sequential()
-model.add(Convolution2D(nb_filters1, conv1_size, conv1_size, border_mode ='same', input_shape=(img_height, img_width , 3)))
+model.add(Convolution2D(nb_filters1, conv1_size, conv1_size, padding ='same', input_shape=(img_height, img_width , 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
 
-model.add(Convolution2D(nb_filters2, conv2_size, conv2_size, border_mode ="same"))
+model.add(Convolution2D(nb_filters2, conv2_size, conv2_size, padding ="same"))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size), data_format="channels_first"))
 
-model.add(Convolution2D(nb_filters3, conv3_size, conv3_size, border_mode ='same'))
+model.add(Convolution2D(nb_filters3, conv3_size, conv3_size, padding ='same'))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size), data_format="channels_first"))
 
 model.add(Flatten())
 model.add(Dense(1024))
@@ -70,7 +72,7 @@ model.add(Dense(classes_num, activation='softmax'))
 
 model.summary()
 model.compile(loss='categorical_crossentropy',
-                      optimizer=optimizers.rmsprop(),
+                      optimizer=keras.optimizers.RMSprop(learning_rate=0.01),
                       metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
@@ -99,7 +101,7 @@ validation_generator = test_datagen.flow_from_directory(
 """
 Tensorboard log
 """
-target_dir = "./models/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+target_dir = "../models/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 if not os.path.exists(target_dir):
   os.mkdir(target_dir)
 model.save('./src/models/model.h5')
@@ -108,7 +110,7 @@ model.save_weights('./src/models/weights.h5')
 checkpoint = ModelCheckpoint(target_dir, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
-model.fit_generator(
+model.fit(
     train_generator,
     steps_per_epoch=nb_train_samples//batch_size,
     epochs=epochs,
@@ -116,5 +118,4 @@ model.fit_generator(
     validation_data=validation_generator,
     callbacks=callbacks_list,
     validation_steps=nb_validation_samples//batch_size)
-
 
